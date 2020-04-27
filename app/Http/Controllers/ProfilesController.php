211472fbb5a8e2a85b104befca49184956d83c5c
Mason <sorom2@student.op.ac.nz>
2020-04-28 10:20:01 +1200

    Add edit and update funcs to ProfilesController

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,12 +9,24 @@ class ProfilesController extends Controller
 {
     public function show($name)
     {
-       $user = User::whereName($name)->first();
-     
-            return view('profile')->withUser($user);
-        
-       
+        $user = User::whereName($name)->first();
+        return view('profile')->withUser($user);
     }
 
-    
+    public function edit($id) {
+        $user = User::find($id);
+        return view('profile.edit')->withUser($user);
+    }
+
+    public function update(Request $request, User $user) {
+        $request->validate([
+            'username'=>['required', 'string', 'max:255', 'unique:users'],
+            'email'=>['required', 'string', 'email', 'max:255', 'unique:users'],
+            'gender'=>['required', 'string', 'max:16'],
+            'biography'=>['required', 'string', 'max:256']
+        ]);
+
+        $user->update($request);
+        return redirect('profile/'.$user->username)->with('success', 'Profile Updated Successfully');
+    }
 }
